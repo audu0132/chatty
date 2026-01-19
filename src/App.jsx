1d94c3b5211aa642a3ed3d3433d5a748c9f813c6
Audumbar More <audumbarmore23@gmail.com>
2026-01-19 11:15:35 +0530

    feat: add routing, new pages, and assets for chat application

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import ProfileUpdate from './pages/ProfileUpdate/ProfileUpdate'
+import Chat from './pages/Chat/Chat'
+import Login from './pages/Login/Login'
 
 const App = () => {
   return (
-    <div>
-      
-    </div>
+    <>
+      <Routes>
+        <Route path='/' element={<Login />} />
+        <Route path='/profile' element={<ProfileUpdate />} />
+        <Route path='/chat' element={<Chat />} />
+      </Routes>
+    </>
   )
 }
 
---
+++ src/pages/Chat/Chat.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './Chat.css'
+const Chat = () => {
+  return (
+    <div>
+      we are in chat page
+    </div>
+  )
+}
+
+export default Chat
